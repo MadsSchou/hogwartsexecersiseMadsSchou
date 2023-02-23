f9e09c9c5a3fb8a5f6910b484493ae192b67c5ea
Mads Schou Olesen <mschouolesen@gmail.com>
2023-02-23 13:57:22 +0100

    opryddet html og js, arrayet dukker op, ingen img

--- script.js
+++ script.js
@@ -1,182 +1,96 @@
+"use strict";
+
 window.addEventListener("DOMContentLoaded", start);
-const globalProps = { chosenFilter: "*" };
-// const allStudents = [];
-
-async function getData(url) {
-  const response = await fetch(url);
-  const jsonData = await response.json();
-  return jsonData;
-}
-
-function cleanUpData(studentsList, i) {
-  // console.log(studentsList);
-
-  // Student object
-  const Student = {
-    studentId: null,
-    firstName: "",
-    lastName: "",
-    middleName: "",
-    nickName: "",
-    gender: "",
-    imgSrc: "",
-    house: "",
-    // blood: "",
-    // prefect: false,
-    // expelled: false,
-    // inqSquad: false,
-    // hacker: null,
-  };
-
-  const student = Object.create(Student);
-}
 
 function start() {
   console.log("ready");
-
-  addEventListeners();
-  loadJSON();
-}
-function addEventListeners() {
-  document.querySelector(`[data-filter="hufflepuff"]`).addEventListener("click", klikFilter);
-  document.querySelector(`[data-filter="rawenclaw"]`).addEventListener("click", klikFilter);
-  document.querySelector(`[data-filter="gryffindor"]`).addEventListener("click", klikFilter);
-  document.querySelector(`[data-filter="slytherin"]`).addEventListener("click", klikFilter);
-  document.querySelector(`[data-filter="*"]`).addEventListener("click", klikFilter);
+  getData();
 }
 
-function klikFilter(evt) {
-  // console.log("klikFilter", evt.target.dataset.filter);
-  globalProps.chosenFilter = evt.target.dataset.filter;
-  buildList();
-}
+const allStudents = [];
+const Student = {
+  firstName: null,
+  lastName: null,
+  middleName: null,
+  nickName: null,
+  image: null,
+  house: null,
+};
 
-async function loadJSON() {
-  const response = await fetch("student.json");
-  const jsonData = await response.json();
+async function getData() {
+  const response = await fetch("https://petlatkea.dk/2021/hogwarts/students.json");
+  const data = await response.json();
   console.log(data);
-
-  // when loaded, prepare data objects
-  prepareObjects(jsonData);
+  prepareObjects(data);
+  console.table(allStudents);
+  displayList();
 }
 
-function prepareObjects(jsonData) {
-  allStudents = jsonData.map(preapareObject);
+function prepareObjects(data) {
+  data.forEach((jsonObject) => {
+    const student = Object.create(Student);
 
-  buildList();
+    const nameArray = jsonObject.fullname.trim().split(" ");
+    let firstName, middleName, lastName, nickName;
+    const nickNameIndex = nameArray.findIndex((name, index) => {
+      return name.startsWith('"') && name.endsWith('"');
+    });
+    if (nickNameIndex !== -1) {
+      nickName = nameArray[nickNameIndex].replaceAll(`"`, "");
+      nickName = nickName.charAt(0).toUpperCase() + nickName.slice(1).toLowerCase();
+      nameArray.splice(nickNameIndex, 1);
+    }
+    if (nameArray.length === 1) {
+      firstName = nameArray[0];
+    } else if (nameArray.length === 2) {
+      firstName = nameArray[0];
+      lastName = nameArray[1];
+    } else {
+      firstName = nameArray[0];
+      middleName = nameArray.slice(1, -1).join(" ");
+      lastName = nameArray[nameArray.length - 1];
+    }
+
+    console.log(nickNameIndex);
+
+    if (lastName) {
+      student.lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
+    }
+
+    student.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+
+    if (middleName) {
+      student.middleName = middleName.charAt(0).toUpperCase() + middleName.slice(1).toLowerCase();
+    }
+    if (nickName) {
+      student.nickName = nickName.charAt(0).toUpperCase() + nickName.slice(1).toLowerCase();
+    }
+
+    student.imgSrc = `./images/${firstName.substring(0, firstName.indexOf(" ")).toLowerCase()}_.png`;
+    student.imgSrc = `./images/${firstName.substring(firstName.lastIndexOf(" ") + 1, firstName.lastIndexOf(" ") + 2).toLowerCase() + firstName.substring(firstName.lastIndexOf(" ") + 2).toLowerCase()}_${firstName.substring(0, 1).toUpperCase().toLowerCase()}.png`;
+    // student.imgSrc = `./images/${jsonObject.fullname.trim().toLowerCase().replace(/\s/g, "_")}.png`;
+
+    student.house = jsonObject.house.trim().charAt(0).toUpperCase() + jsonObject.house.trim().slice(1).toLowerCase();
+    allStudents.push(student);
+  });
 }
-function buildList() {
-  console.log("BUILD LIST");
-  const currentList = filterList(allStudents);
+displayList();
 
-  displayList(currentList);
+function displayList() {
+  document.querySelector("#list tbody").innerHTML = "";
+  allStudents.forEach(displayStudent);
 }
 
-function filterList(theFilteredList) {
-  if (globalProps.chosenFilter === "hufflepuff") {
-    theFilteredList = allStudents.filter(isHufflepuff);
-  } else if (globalProps.chosenFilter === "rawenclaw") {
-    theFilteredList = allStudents.filter(isRawenclaw);
-  }
-  return theFilteredList;
+function displayStudent(student) {
+  const clone = document.querySelector("template#student").content.cloneNode(true);
+  clone.querySelector("[data-field=firstname]").innerHTML = student.firstName;
+  clone.querySelector("[data-field=middlename]").innerHTML = student.middleName;
+  clone.querySelector("[data-field=nickname]").innerHTML = student.nickName;
+  clone.querySelector("[data-field=lastname]").innerHTML = student.lastName;
+  clone.querySelector("[data-field=house]").innerHTML = student.house;
+  clone.querySelector("[data-field=image]").innerHTML = student.image;
+
+  // clone.querySelector("[data-field=student_img]").src = student.imgSrc;
+  // clone.querySelector("[data-field=student_img]").alt = `Picture of ${student.firstName} ${student.lastName}`;
+  document.querySelector("#list tbody").appendChild(clone);
 }
-
-function isHufflepuff(student) {
-  if (student.type === "hufflepuff") {
-    return true;
-  }
-}
-function isRawenclaw(student) {
-  if (student.type === "rawenclaw") {
-    return true;
-  }
-}
-function isGryffindor(student) {
-  if (student.type === "gryffindor") {
-    return true;
-  }
-}
-function isSlytherin(student) {
-  if (student.type === "slytherin") {
-    return true;
-  }
-}
-
-function preapareObject(jsonObject) {
-  const student = Object.create(Student);
-
-  const texts = jsonObject.fullname.split(" ");
-  student.name = texts[0];
-  student.desc = texts[2];
-  student.type = texts[3];
-  student.age = jsonObject.age;
-
-  return student;
-}
-
-// let data;
-// let filter = "alle";
-
-// const filterKnapper = document.querySelectorAll("nav li");
-// filterKnapper.forEach((knap) => knap.addEventListener("click", filtrerKategorier));
-// hentData();
-
-// function filtrerKategorier() {
-//   filter = this.dataset.kategori;
-//   document.querySelector(".valgt").classList.remove("valgt");
-//   this.classList.add("valgt");
-//   vis(data);
-// }
-
-// async function hentData() {
-//   const respons = await fetch("https://petlatkea.dk/2021/hogwarts/students.json");
-//   data = await respons.json();
-//   console.log(data);
-//   vis(data);
-// }
-
-// function vis(data) {
-//   const main = document.querySelector("main");
-//   const template = document.querySelector("template").content;
-//   main.textContent = "";
-//   data.forEach((student) => {
-//     if (filter == student.kategori || filter == "alle") {
-//       const klon = template.cloneNode(true);
-//       klon.querySelector("article").addEventListener("click", () => visStudent(student));
-//       klon.querySelector(".billedeurl").src = "medium/" + student.billednavn + "-md.jpg";
-//       klon.querySelector(".firstname").textContent = student.firstname;
-//       klon.querySelector(".lastname").textContent = student.lastname;
-//       main.appendChild(klon);
-//     }
-//   });
-// }
-// function visStudent(studentData) {
-//   console.log(studentData);
-//   const popop = document.querySelector("#popop");
-//   popop.style.display = "flex";
-//   popop.querySelector(".billedeurl").src = "medium/" + studentData.billednavn + "-md.jpg";
-//   //   popop.querySelector("h3").textContent = studentData.navn;
-//   popop.querySelector(".firstname").textContent = studentData.firstname;
-//   popop.querySelector(".lastname").textContent = studentData.lastname;
-//   popop.querySelector(".middlename").textContent = studentData.middlename;
-//   popop.querySelector(".nickname").textContent = studentData.nickname;
-//   popop.addEventListener("click", () => (popop.style.display = "none"));
-// }
-
-// hentData();
-// /*Burgermenu*/
-// const btn = document.querySelector(".toggle-btn");
-// const menu = document.querySelector(".main-menu");
-// const menuimg = btn.querySelector("img");
-// function toggleMenu() {
-//   menu.classList.toggle("shown");
-//   const menuShown = menu.classList.contains("shown");
-//   if (menuShown) {
-//     console.log(menuShown);
-//     menuimg.src = "medium/luk.svg";
-//   } else {
-//     console.log(menuShown);
-//     menuimg.src = "medium/burgermenu.svg";
-//   }
-// }
-// btn.addEventListener("click", toggleMenu);
